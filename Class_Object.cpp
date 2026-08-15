c32e42e56b8338d756b7ecb952c8a58287bce898
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-08-15 11:57:57 +0600

    Update Class_Object.cpp

--- Class_Object.cpp
+++ Class_Object.cpp
@@ -53,3 +53,34 @@ cout<<a.name<<" "<<a.gpa<<" "<<a.roll<<endl;
 cout<<b.name<<" "<<b.gpa<<" "<<b.roll<<endl;
     return 0;
 }
+
+
+
+// class and object input with space 
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+public:
+char name[100];
+int roll;
+double gpa;
+};
+
+int main(){
+
+Student a,b;
+cin.getline(a.name,100);
+cin>>a.gpa>>a.roll;
+cin.ignore();
+cin.getline(b.name,100);
+cin>>b.gpa>>b.roll;
+
+
+cout<<a.name<<" "<<a.gpa<<" "<<a.roll<<endl;
+cout<<b.name<<" "<<b.gpa<<" "<<b.roll<<endl;
+    return 0;
+}
