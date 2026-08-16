a3b5647af23201811061806d1ed4c1492570600e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-08-17 00:02:54 +0600

    Refactor Class_Object.cpp for consistency and clarity

--- Class_Object.cpp
+++ Class_Object.cpp
@@ -84,3 +84,36 @@ cout<<a.name<<" "<<a.gpa<<" "<<a.roll<<endl;
 cout<<b.name<<" "<<b.gpa<<" "<<b.roll<<endl;
     return 0;
 }
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    float height;
+    char grade;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter your height: ";
+    cin >> height;
+
+    cout << "Enter your grade: ";
+    cin >> grade;
+
+    cout << "Age = " << age << endl;
+    cout << "Height = " << height << endl;
+    cout << "Grade = " << grade << endl;
+
+    return 0;
+}
