751fad81f365e3ce1e7cdd73c97933f26d155846
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-04 23:32:56 +0600

    Update Sraddha.cpp

--- Sraddha.cpp
+++ Sraddha.cpp
@@ -24,3 +24,37 @@ t1.salary = 25000;
 cout<<t1.name<<" "<<t1.dept<<" "<<t1.salary<<endl;
     return 0;
 }
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+
+    string name;
+    int StdID;
+    double gpa;
+
+};
+
+
+int main(){
+
+Student tanim,afif;
+tanim.name = "Mahmud Tanim";
+tanim.StdID = 252311098;
+tanim.gpa = 4.54;
+
+
+afif.name = "Afif Mahmud";
+afif.StdID = 311098;
+afif.gpa = 5.54;
+
+cout<<tanim.name<<" "<<tanim.StdID<<" "<<tanim.gpa<<endl;
+cout<<afif.name<<" "<<afif.StdID<<" "<<afif.gpa<<endl;
+
+return 0;
+}
