b69cad9013a85c8e83cb20ad98eeda36edb1dc7c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-11 17:39:57 +0600

    Add files via upload

---
+++ Object_return_from_function.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+public:
+int roll;
+int id;
+int cls;
+
+Student(int roll,int id,int cls){
+this->roll = roll;
+this->id = id;
+this->cls = cls;
+
+}
+
+
+};
+
+Student fun(){
+    Student Rahim(10,20,30);
+    return Rahim;
+}
+
+int main(){
+
+
+
+Student obj = fun();
+cout<<obj.roll<<" "<<obj.id<<" "<<obj.cls<<endl;
+
+
+}
